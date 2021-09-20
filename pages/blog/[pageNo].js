17e0933bc66d52cf40c038bb7417f1bca0518c39
thapatechnical <vbthapa55@gmail.com>
2021-09-20 11:39:46 +0530

    added data fetching methods

--- pages/blog/[pageNo].js
+++ pages/blog/[pageNo].js
@@ -1,17 +1,54 @@
-import { useRouter } from "next/router";
+import Navbar from "../../components/Navbar";
 
-const pageNo = () => {
-  const router = useRouter();
-  const pageNumber = router.query.pageNo;
+export const getStaticPaths = async () => {
+  const res = await fetch("https://jsonplaceholder.typicode.com/posts");
+  const data = await res.json();
+
+  const paths = data.map((curElem) => {
+    return {
+      params: {
+        pageno: curElem.id.toString(),
+      },
+    };
+  });
+
+  return {
+    paths,
+    fallback: false,
+  };
+};
+
+export const getStaticProps = async (context) => {
+  const id = context.params.pageno;
+  const res = await fetch(`https://jsonplaceholder.typicode.com/posts/${id}`);
+  const data = await res.json();
+
+  return {
+    props: {
+      data,
+    },
+  };
+};
+
+const myData = ({ data }) => {
+  const { id, title, body } = data;
   return (
     <>
-      <h1>my {pageNumber} content </h1>
+      <Navbar />
+      <div className="ssr-styles ssr-styles-inside">
+        <h3>{id}</h3>
+        <h2>{title}</h2>
+        <p>{body}</p>
+      </div>
     </>
   );
 };
 
-export default pageNo;
+export default myData;
 
 // What will happend if the file is already exist in our folder and we tried to call the pages dynamic route ?
 
 // Which page will be serve by the next.js app?
+
+// getStaticPaths defines which pages next.js has to render when exporting.
+// It is used to generate all available dynamic routes.
--- pages/blog/index.js
+++ pages/blog/index.js
@@ -1,9 +1,31 @@
 import Navbar from "../../components/Navbar";
-const blog = () => {
+import Link from "next/link";
+
+export const getStaticProps = async () => {
+  const res = await fetch("https://jsonplaceholder.typicode.com/posts");
+  const data = await res.json();
+
+  return {
+    props: {
+      data,
+    },
+  };
+};
+
+const blog = ({ data }) => {
   return (
     <>
       <Navbar />
-      <h1>Hello World my blog </h1>
+      {data.slice(0, 5).map((curElem) => {
+        return (
+          <div key={curElem.id} className="ssr-styles">
+            <h3>{curElem.id}</h3>
+            <Link href={`/blog/${curElem.id}`}>
+              <h2>{curElem.title}</h2>
+            </Link>
+          </div>
+        );
+      })}
     </>
   );
 };
